664be68dccdf373ee89d5d51d4fa2f6d9c0b8d12
yashtanwar1511 <yashtanwar1511@gmail.com>
2026-07-06 13:49:56 +0530

    Added JUnit Exercise 1 - Setting Up JUnit

---
+++ Java FSE/Deepskilling/JUnit, Mockito and SL4J/Exercise1/SampleTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SampleTest {
+
+    @Test
+    public void testAddition() {
+
+        int result = 10 + 20;
+
+        assertEquals(30, result);
+
+    }
+}
