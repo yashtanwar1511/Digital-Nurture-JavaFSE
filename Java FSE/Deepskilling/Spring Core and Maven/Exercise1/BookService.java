d0bd6618b876decd60b1099e53189a116d29567d
yashtanwar1511 <yashtanwar1511@gmail.com>
2026-07-07 08:52:02 +0530

    Added Spring Core Exercise 1 - Configuring a Basic Spring Application

---
+++ Java FSE/Deepskilling/Spring Core and Maven/Exercise1/BookService.java
@@ -0,0 +1,21 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void displayService() {
+
+        System.out.println("Book Service Created");
+
+        bookRepository.displayRepository();
+
+    }
+
+}
---
+++ Java FSE/Deepskilling/Spring Core and Maven/Exercise1/Main.java
@@ -0,0 +1,19 @@
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService service =
+                (BookService) context.getBean("bookService");
+
+        service.displayService();
+
+    }
+
+}
